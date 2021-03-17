6ccf804d0696f99440408784d890c5bb8384dca0
Riller <rvsriller@gmail.com>
2021-03-17 03:01:48 -0300

    Dino Game

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const dino = document.querySelector('.dino');
 const background = document.querySelector('.background'); //Background div to add cactus 
 
-let isJumping = false; //Prevemt dino's shaking
+let isJumping = false; //Prevent dino's shaking
 let isGameOver = false;
 let position = 0;
 
@@ -45,18 +45,21 @@ function createCactus() {
     let cactusPosition = 1000 //Position from right to left side
     let randomTime = Math.random() * 6000;
 
+    if (isGameOver) return;
+
     cactus.classList.add('cactus');
-    cactus.style.left = 1000 + 'px';
+    cactus.style.left = cactusPosition + 'px';
     background.appendChild(cactus);
 
-    let leftInterval = setInterval(() => {
+    let leftTimer = setInterval(() => {
         //if cactus disappear, do something (-60px is the position where it disappears)
         if(cactusPosition < -60){
-            clearInterval(leftInterval);
+            clearInterval(leftTimer);
             background.removeChild(cactus);
         } else if (cactusPosition > 0 && cactusPosition < 60 && position < 60) {
             //Game over 
-            clearInterval(leftInterval);
+            clearInterval(leftTimer);
+            isGameOver = true;
             document.body.innerHTML = '<h1 class="game-over">Fim de jogo</h1>';
         } else {
             cactusPosition -= 10;
